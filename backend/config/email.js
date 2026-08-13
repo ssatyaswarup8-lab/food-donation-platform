cb92e803c64b96ce49158c37dc3bad669adc1cfc
Satyaswarup Sahoo <ssatyaswarup8@gmail.com>
2026-08-13 23:28:43 +0530

    added

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,15 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  service: "gmail",
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
